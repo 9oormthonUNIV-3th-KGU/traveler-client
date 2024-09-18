9c05ce96bfe4ec5d31d298ca024b6c4e9291e7f0
jihyeoniiiii <105184159+Jihyeoniiiii@users.noreply.github.com>
2024-09-18 18:52:23 +0900

    design: onboarding box shadow 변경

--- src/components/ui/on-boarding-box.tsx
+++ src/components/ui/on-boarding-box.tsx
@@ -1,9 +1,15 @@
-function OnBoardingBox({ title, message }: { title: string; message: string }) {
+function OnBoardingBox({
+  title,
+  message,
+}: {
+  title: string
+  message: React.ReactNode
+}) {
   return (
-    <div className="mt-20 flex h-screen items-center justify-center">
-      <div className="relative z-10 h-1/4 w-5/6 rounded-2xl p-10 text-center shadow-xl">
-        <span className="mb-7 block text-3xl">{title}</span>
-        <span className="text-xl">{message}</span>
+    <div className="flex h-screen items-center justify-center">
+      <div className="relative z-10 mt-[130px] h-[145px] w-[335px] rounded-2xl p-[25px] text-center shadow-all-sides">
+        <span className="mb-4 block text-2xl">{title}</span>
+        <span className="text-base">{message}</span>
       </div>
     </div>
   )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -43,6 +43,10 @@ const config: Config = {
           300: '#067DFD',
         },
       },
+      boxShadow: {
+        'all-sides':
+          '0 4px 6px rgba(0, 0, 0, 0.05), 0 -4px 6px rgba(0, 0, 0, 0.05), 4px 0 6px rgba(0, 0, 0, 0.05), -4px 0 6px rgba(0, 0, 0, 0.05)',
+      },
     },
   },
   plugins: [],
