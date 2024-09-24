68f7d6bd026d1fb9d084505d136ad287d83ff86d
jihyeoniiiii <105184159+Jihyeoniiiii@users.noreply.github.com>
2024-09-25 00:05:13 +0900

    design: bgColor, outline 변경

--- src/components/search-container.tsx
+++ src/components/search-container.tsx
@@ -32,7 +32,7 @@ export default function SearchContainer({
 
   return (
     <div
-      className={`fixed z-10 h-[800px] w-[640px] translate-x-[-50%] rounded-2xl bg-gray-200 p-10 shadow transition-transform duration-500 ease-in-out ${isOpen ? 'visible translate-y-0 opacity-100' : 'invisible translate-y-full opacity-0'}`}
+      className={`fixed z-10 h-[800px] w-[640px] translate-x-[-50%] rounded-2xl bg-gray-50 p-10 shadow transition-transform duration-500 ease-in-out ${isOpen ? 'visible translate-y-0 opacity-100' : 'invisible translate-y-full opacity-0'}`}
       style={{ left: '50%', bottom: 0 }}
     >
       <div className="relative w-full">
@@ -41,7 +41,7 @@ export default function SearchContainer({
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
           placeholder={selectedInput === 0 ? '출발지 입력' : '도착지 입력'}
-          className="h-28 w-full rounded-3xl border-gray-300 p-10 text-3xl shadow"
+          className="h-28 w-full rounded-3xl border-gray-300 p-10 text-3xl shadow outline-none"
         />
         <button onClick={handleClick} className="absolute right-10 mt-10">
           {searchIcon}
