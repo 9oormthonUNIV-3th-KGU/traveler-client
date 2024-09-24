5e3cba838409340d539e5be1dd9820c775377cf1
jihyeoniiiii <105184159+Jihyeoniiiii@users.noreply.github.com>
2024-09-24 23:30:04 +0900

    design: logo 변경

--- src/components/top-bar.tsx
+++ src/components/top-bar.tsx
@@ -1,15 +1,18 @@
 import Image from 'next/image'
 import userIcon from '~/assets/images/user.png'
+import Logo from '~/assets/logo.svg'
 
 export default function TopBar() {
+  const logoIcon = <Image src={Logo} alt="logo" width={100} />
+
   return (
     <div className="relative mb-5 flex h-20 w-full flex-row p-5">
-      <span className="text-5xl text-primary-400">산책</span>
+      <div className="text-5xl text-primary-400">{logoIcon}</div>
       <Image
         className="absolute right-5 top-6"
         src={userIcon}
         alt="user"
-        width={40}
+        width={50}
       />
     </div>
   )
