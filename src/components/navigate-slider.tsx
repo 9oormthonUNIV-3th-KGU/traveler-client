75b662bb0a9a40b112b41a0e5e47f2a79c6eefdb
조성원 <tjddnjsjo@kakao.com>
2024-09-25 00:23:58 +0900

    feat: update navigate-slider component to use embla-carousel-react

--- src/components/navigate-slider.tsx
+++ src/components/navigate-slider.tsx
@@ -1,11 +1,22 @@
+'use client'
+
+import useEmblaCarousel from 'embla-carousel-react'
+
 import { NavigateCard } from '~/components/navigate-card'
 import { NavigateType } from '~/constants/navigate-type'
 import type { Feature } from '~/types/feature'
 
 function NavigateSlider({ features }: { features: Feature[] }) {
+  const [emblaRef] = useEmblaCarousel({
+    containScroll: false,
+  })
+
   return (
-    <div className="absolute bottom-5 z-50 w-full overflow-x-scroll px-5">
-      <div className="flex w-full justify-center gap-2">
+    <div
+      className="absolute bottom-3 z-50 w-full overflow-hidden p-5"
+      ref={emblaRef}
+    >
+      <div className="flex w-full gap-2">
         {features
           .filter(
             (feature) =>
