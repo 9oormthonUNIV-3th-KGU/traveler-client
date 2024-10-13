6fb97e66b7ff49f7687f753c5ed461758ed0b905
조성원 <tjddnjsjo@kakao.com>
2024-10-13 19:30:28 +0900

    fix: 기획 수정으로 인한 페이지 순서 재배치

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,6 @@
-import Image from 'next/image'
-
-import LogoMain from '~/assets/logo+main.svg'
-import { LoginButton } from '~/components/login-button'
-import { OnBoardIndicator } from '~/components/on-board-indicator'
-import { Card, CardDescription, CardTitle } from '~/components/ui/card'
-
 export default function Home() {
   return (
     <div className="flex min-h-dvh flex-col justify-center px-5">
-      <div className="relative flex w-full flex-col items-center">
-        <Image src={LogoMain} alt="산책" width={640} draggable={false} />
-        <Card className="absolute bottom-0 w-full items-center gap-3">
-          <CardTitle>간편 로그인으로 산책 시작</CardTitle>
-          <CardDescription>
-            카카오톡 계정으로 빠른 로그인을 도와드릴게요!
-          </CardDescription>
-        </Card>
-      </div>
-      <div className="mb-2 mt-6 flex w-full items-center justify-center p-2">
-        <OnBoardIndicator step={0} />
-      </div>
-      <LoginButton />
     </div>
   )
 }
