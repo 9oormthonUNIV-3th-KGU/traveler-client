48c1b9e8c56626eefeec1314a05d33ead9cfde78
조성원 <tjddnjsjo@kakao.com>
2024-10-13 19:31:00 +0900

    fix: 기획 수정으로 인한 페이지 순서 재배치

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,3 @@
 export default function Home() {
-  return (
-    <div className="flex min-h-dvh flex-col justify-center px-5">
-    </div>
-  )
+  return <div className="flex min-h-dvh flex-col justify-center px-5"></div>
 }
