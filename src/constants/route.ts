7e2ee322ac6fb7e0fd68f65fb117709a65f0cba0
조성원 <tjddnjsjo@kakao.com>
2024-10-14 18:34:07 +0900

    fix: main route 수정

--- src/constants/route.ts
+++ src/constants/route.ts
@@ -1,7 +1,7 @@
 const ROUTE = {
   LOGIN: '/login',
   KAKAO_LOGIN: process.env.NEXT_PUBLIC_OAUTH_URL,
-  MAIN: '/home',
+  MAIN: '/',
   NAVIGATE: '/navigate',
   ONBOARD: '/on-board',
   MYPAGE: '/my-page',
