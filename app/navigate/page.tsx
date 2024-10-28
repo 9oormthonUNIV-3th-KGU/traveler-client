8d38623e494c42a2d1fa5ae1eb0ca1df3124172f
조성원 <tjddnjsjo@kakao.com>
2024-10-29 04:48:52 +0900

    feat: 경로 path 추가

--- app/navigate/page.tsx
+++ app/navigate/page.tsx
@@ -67,11 +67,7 @@ export default function Navigate({
           time={features[0].properties.totalTime}
           distance={features[0].properties.totalDistance}
         />
-        <TMap
-          // features={features}
-          from={start}
-          to={end}
-        />
+        <TMap features={features} from={start} to={end} />
         <NavigateSlider features={features} />
       </div>
     )
